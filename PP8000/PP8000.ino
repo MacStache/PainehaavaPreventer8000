60bf19018dce4bd50562d573942d9f8a587299be
MacStache <123492715+MacStache@users.noreply.github.com>
2023-03-21 20:09:59 +0200

    Update PP8000.ino
    Koodi kommentoitu selkeyttä varten.

--- PP8000/PP8000.ino
+++ PP8000/PP8000.ino
@@ -1,20 +1,23 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(1,2,3,4,5,6);
+LiquidCrystal lcd(1,2,3,4,5,6); //määritellään käytettävät LCD-portit. 
+                                //Portit 1-2 on tarkoitettu R/S (Register Select) ja E (Enable) porteille ja 3-6 porteille joista syötetään bittejä näytölle.
+                                //R/S-portti on portti jonka kautta näytölle syötetään komentoja
+                                //E-portti on portti joka avaa rekisterin kirjoitusta varten
 
 void setup() {
-  // put your setup code here, to run once:
-  lcd.begin(16,2);
+
+  lcd.begin(16,2); //määritellään LCD-näytön mitat (16x2 -merkkiä)
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  lcd.print("Kylkiasentoon!");
-  delay(3000);
+  
+  lcd.print("Kylkiasentoon!"); //Tulostetaan näytölle teksti
+  delay(3000); //Odotetaan 3 sekuntia
 
-  lcd.setCursor(2,1);
-  lcd.print("PP8000");
-  delay(3000);
+  lcd.setCursor(2,1);// Siirretään kursori sarakkeeseen 3 (lasketaan nollasta) ja riville 2 (lasketaan nollasta)
+  lcd.print("PP8000"); //tulostetaan teksti
+  delay(3000); //odotetaan 3 sekuntia
 
-  lcd.clear();
+  lcd.clear(); //tyhjennetään LCD-näyttö
 }

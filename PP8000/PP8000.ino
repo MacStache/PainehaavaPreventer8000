ab6bc91e7e5dfdda69bf0d54b61caa2abfe1e75c
Rapuska <118633578+Rapuska@users.noreply.github.com>
2023-03-23 20:01:49 +0200

    Update PP8000.ino

--- PP8000/PP8000.ino
+++ PP8000/PP8000.ino
@@ -7,6 +7,7 @@ LiquidCrystal lcd(1,2,3,4,5,6); //määritellään käytettävät LCD-portit.
 
 void setup() {
 
+  Serial.begin(9600);
   lcd.begin(16,2); //määritellään LCD-näytön mitat (16x2 -merkkiä)
 }
 
@@ -20,4 +21,15 @@ void loop() {
   delay(3000); //odotetaan 3 sekuntia
 
   lcd.clear(); //tyhjennetään LCD-näyttö
+
+  float alkuArvo = analogRead(A0);
+  float massa = (alkuArvo - 511)*125.0/6.0; // Ei mitään hajua mitä tässä lasketaan, otin youtubesta
+
+  Serial.print("Massa: ");
+  Serial.print(massa);  // Näyttää vielä mitä sattuu, pitää kalibroida
+  Serial.print(" kg");
+  Serial.print("	V: ");
+  Serial.print(analogRead(A0)*5.0/1023.0);  // Lasketaan analogipinnin saama jännite
+  Serial.println("V");
+  delay(10);
 }

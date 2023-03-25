ff0330244bd8518d449e67ec80449eb6edaafa75
MacStache <123492715+MacStache@users.noreply.github.com>
2023-03-25 14:44:42 +0200

    Update PP8000.ino

--- PP8000/PP8000.ino
+++ PP8000/PP8000.ino
@@ -1,6 +1,10 @@
 #include <LiquidCrystal.h>
+float alkuArvo = analogRead(A0); //analogipinnistä saatava lukema, siirsin tämän loopin ulkopuolelle
+bool mittaus = false; //mittauskytkin
+bool halytys = false; //hälytyskytkin
+int ajastin = 0; //ajastimen muuttuja
 
-LiquidCrystal lcd(1,2,3,4,5,6); //määritellään käytettävät LCD-portit. 
+LiquidCrystal lcd(2,3,4,5,6,7); //määritellään käytettävät LCD-portit. 
                                 //Portit 1-2 on tarkoitettu R/S (Register Select) ja E (Enable) porteille ja 3-6 porteille joista syötetään bittejä näytölle.
                                 //R/S-portti on portti jonka kautta näytölle syötetään komentoja
                                 //E-portti on portti joka avaa rekisterin kirjoitusta varten
@@ -12,7 +16,8 @@ void setup() {
 }
 
 void loop() {
-  
+
+  //LCD-näytön esimerkkikoodia
   lcd.print("Kylkiasentoon!"); //Tulostetaan näytölle teksti
   delay(3000); //Odotetaan 3 sekuntia
 
@@ -21,15 +26,37 @@ void loop() {
   delay(3000); //odotetaan 3 sekuntia
 
   lcd.clear(); //tyhjennetään LCD-näyttö
+  //LCD-näytön esimerkkikoodi loppuu
 
-  float alkuArvo = analogRead(A0);
-  float massa = (alkuArvo - 511)*125.0/6.0; // Ei mitään hajua mitä tässä lasketaan, otin youtubesta
+  /*if (ajastin >= 500000){ //Hälyttimen placeholder
+    halytys = true;
+  }*/
+  
+  while (alkuArvo >= 0 && mittaus == false){ //Jos massa on suurempi kuin 0 ja jos mittaus ei ole jo käynnissä käynnistetään anturienLuku- ja LCD;n kirjoitusfunktio
+    if (mittaus = false){ //Jos mittaus ei ole jo käynnissä
+      mittaus = true; //Asetetaan mittaus alkaneeksi
+      //int ajastin = 0; //Ajastimen placeholder, ei toiminnallinen. Startataan ajastin kun anturienluku() funktiota kutsutaan
+     
+      float massa = (alkuArvo - 511)*125.0/6.0; // Ei mitään hajua mitä tässä lasketaan, otin youtubesta Henkan kommentti: Tässä lasketaan luultavasti kalibrointi. 
 
-  Serial.print("Massa: ");
-  Serial.print(massa);  // Näyttää vielä mitä sattuu, pitää kalibroida
-  Serial.print(" kg");
-  Serial.print("	V: ");
-  Serial.print(analogRead(A0)*5.0/1023.0);  // Lasketaan analogipinnin saama jännite
-  Serial.println("V");
-  delay(10);
+      lcd.setCursor(0,0); //Siirretään kursori ensimmäisen rivin ensimmäisen merkin kohdalle
+      lcd.print("Massa: "); //Kirjoitetaan näytölle, että mitä näytetään
+      lcd.print(massa);  //Kirjoitetaan massa muuttujan arvo, joka näyttää vielä mitä sattuu, pitää kalibroida
+      //Serial.print(" kg"); //En tiedä onko tarpeellistä lisätä yksikköä
+      lcd.setCursor(0,1); //Siirretään kursori toisen rivin ensimmäisen merkin kohdalle
+      //Näillä alemmillahan ei ole mitään merkitystä, mutta voidaan asettaa niihin toisen anturin tiedot, esim massa2 muuttuja ja toinen analogiportti
+      Serial.print("	V: ");
+      Serial.print(analogRead(A0)*5.0/1023.0);  // Lasketaan analogipinnin saama jännite
+      Serial.println("V");
+      delay(10);
+      }
+    else if (alkuArvo == 0 && mittaus == true){ //jos anturilta saatava lukema on 0 ja mittaus on käynnissä  
+      mittaus = false; //asetetaan mittaus päättyneeksi
+      
+      }
+    } 
+  
+
+  
+
 }

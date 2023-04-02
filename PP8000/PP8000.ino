d7581345d1d59dadf3be5ca479ec2f27f5c1301f
MacStache <123492715+MacStache@users.noreply.github.com>
2023-04-02 10:07:49 +0300

    Update PP8000.ino

--- PP8000/PP8000.ino
+++ PP8000/PP8000.ino
@@ -1,5 +1,21 @@
 #include <LiquidCrystal.h>
-float alkuArvo = analogRead(A0); //analogipinnistä saatava lukema, siirsin tämän loopin ulkopuolelle
+#include <HX711_ADC.h>
+#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+#endif
+
+//pinnit:
+const int HX711_dout = 10; //mcu > HX711 dout pin
+const int HX711_sck = 11; //mcu > HX711 sck pin
+
+//HX711 määrittely:
+HX711_ADC LoadCell(HX711_dout, HX711_sck); //LoadCell funktio saa tietonsa HX711_dout ja _sck pinneistä
+
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
+
+//Mittaukseen liittyvät muuttujat
+float alkuArvo = 0; //Mittauksen aloitusarvo
 bool mittaus = false; //mittauskytkin
 bool halytys = false; //hälytyskytkin
 int ajastin; //ajastimen muuttuja
@@ -11,8 +27,37 @@ LiquidCrystal lcd(2,3,4,5,6,7); //määritellään käytettävät LCD-portit.
 
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(57600); delay(10);
   lcd.begin(16,2); //määritellään LCD-näytön mitat (16x2 -merkkiä)
+
+  //Anturien kalibrointi ja taaraus
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Käynnistetään...");
+
+  LoadCell.begin();
+  //LoadCell.setReverseOutput(); //Kommentti pois jos halutaan muuttaa negatiivinen mittaustulos positiiviseksi
+  unsigned long stabilizingtime = 2000; // Käynnistyksen jälkeistä tarkkuutta voidaan korottaa asettamalla pidempi stabilointiaika (nyt 2000ms/2s)
+  boolean _tare = true; //Aseta tämä false asentoon jos et halua taarata
+
+  //anturien taaraus
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag() || LoadCell.getSignalTimeoutFlag()) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Aikakatkaisu, tarkista johdot"); //jos pinnit on väärin asetettu niin laitteen yhteys aikakatkaistaan
+    lcd.setCursor(0,1);
+    lcd.print("ja pinnien asennot.");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(1.0); // Käyttäjän asetama kalibraatiomääre (float), oletusasetuksena 1.0 mutta voidaan muuttaa tarvittaessa
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.println("Käynnistys valmis.");
+  }
+  //while (!LoadCell.update());
+  //calibrate(); //Aloita kalibrointi
 }
 
 void loop() {
@@ -28,11 +73,14 @@ void loop() {
   lcd.clear(); //tyhjennetään LCD-näyttö
   //LCD-näytön esimerkkikoodi loppuu
 
+
   /*
     if (ajastin >= 500000){ //Hälyttimen placeholder
     halytys = true;
-  }*/
+  }
+  */
   
+
   while (alkuArvo >= 0 && mittaus == false){ //Jos massa on suurempi kuin 0 ja jos mittaus ei ole jo käynnissä käynnistetään anturienLuku- ja LCD;n kirjoitusfunktio
     if (mittaus = false){ //Jos mittaus ei ole jo käynnissä
       mittaus = true; //Asetetaan mittaus alkaneeksi
@@ -56,8 +104,9 @@ void loop() {
       lcd.clear(); //tyhjennetään LCD-näyttö
       }
     } 
-  
-
-  
-
 }
+  
+
+  
+
+

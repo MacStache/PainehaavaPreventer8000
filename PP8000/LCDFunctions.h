8170fa5e1d8fec2d6b676a194c45d1251d1559a6
rahukettu <78539693+rahukettu@users.noreply.github.com>
2023-04-17 09:47:07 +0300

    Update LCDFunctions.h
    Add repair to customChar code

--- PP8000/LCDFunctions.h
+++ PP8000/LCDFunctions.h
@@ -1,54 +1,57 @@
 #include <LiquidCrystal.h>
 //FIXME Fiksataan koko looppi. Pohja on valmiina, mutta ei jostain syystä pelitä (vaihda ääkkösiä Pasin koodin mukaisesti)
-void createCustomChars(LiquidCrystal& lcd) {
 
-  byte AwithDots[8] = {
-    B01010,
-    B00000,
-    B01110,
-    B00001,
-    B01111,
-    B10001,
-    B01111,
+
+void createCustomChars(LiquidCrystal& lcd) {
+  byte AwithDots[] = {
+  B00000,
+  B00000,
+  B01010,
+  B11111,
+  B00001,
+  B11111,
+  B10001,
+  B11111
   };
   
-  byte OwithDots[8] = {
-    B01010,
-    B00000,
-    B01110,
-    B10001,
-    B10001,
-    B10001,
-    B01110,
+  byte OwithDots[] = {
+  B00000,
+  B00000,
+  B01010,
+  B11111,
+  B10001,
+  B10001,
+  B10001,
+  B11111
   };
   
-  byte CapitalAwithDots[8] = {
+  byte CapitalAwithDots[] = {
+   B01010,
+   B00100,
+   B01010,
+   B01010,
+   B01110,
+   B11011,
+   B10001,
+   B10001
+  };
+  
+  byte CapitalOwithDots[] = {
+    B01010,
     B00000,
-    B01110,
-    B10001,
     B11111,
     B10001,
     B10001,
     B10001,
+    B10001,
+    B11111
   };
-  
-  byte CapitalOwithDots[8] = {
-    B00000,
-    B01110,
-    B10001,
-    B10001,
-    B10001,
-    B10001,
-    B01110,
-  };
-  
+
   lcd.createChar(1, AwithDots);         // ä
   lcd.createChar(2, OwithDots);         // ö
   lcd.createChar(3, CapitalAwithDots);  // Ä
   lcd.createChar(4, CapitalOwithDots);  // Ö
 }
-
-createCustomChars(lcd);
 
 void lcdFunc(LiquidCrystal& lcd, uint8_t col, uint8_t row, const char message[]) { //LCD funktioiden wrapperi. Annetaan sille kirjasto, sarakkeet, rivit ja viesti käsiteltäväksi.
   if (col == 255 && row == 255) { //Sarake 255 ja Rivi 255 tyhjentää näytön
@@ -59,16 +62,16 @@ void lcdFunc(LiquidCrystal& lcd, uint8_t col, uint8_t row, const char message[])
   }
   for(int i = 0; i < strlen(message); i++){
   if (message[i] == 'ä'){
-    lcd.write(1);
+    lcd.write((uint8_t)1);
   }
   else if (message[i] == 'ö') {
-    lcd.write(2);
+    lcd.write((uint8_t)2);
   }
   else if (message[i] == 'Ä') {
-    lcd.write(3); 
+    lcd.write((uint8_t)3); 
   }
   else if (message[i] == 'Ö') {
-    lcd.write(4); 
+    lcd.write((uint8_t)4); 
   }
   else {
     lcd.print(message[i]); //Kirjoitetaan viesti

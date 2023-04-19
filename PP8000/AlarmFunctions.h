ef1d6db99a7f260222f39ad3df72e5b370df806f
Rapuska <118633578+Rapuska@users.noreply.github.com>
2023-04-19 17:30:12 +0300

    Käynnistys- ja error-äänet lisätty

--- PP8000/AlarmFunctions.h
+++ PP8000/AlarmFunctions.h
@@ -13,3 +13,29 @@ void SetupAlarm() {  //luodaan halytysfunktio
   }while(i<3); //kun halytysten maaratty arvo on saavutettu poistutaan loopista
   alarm = false; // ja halytyksen arvoksi tulee (=epatosi), tama koodi siis halyttaa kolmesti
 }
+
+void startUpSound() {
+  tone(buzzerPin, 1000, 100); 
+  delay(100);
+  tone(buzzerPin, 1250, 100); 
+  delay(100);
+  tone(buzzerPin, 1500, 100); 
+  delay(100);
+  tone(buzzerPin, 2000, 400);  
+}
+
+void errorSound() {
+  tone(buzzerPin, 2000, 100); 
+  delay(100);
+  tone(buzzerPin, 1500, 100); 
+  delay(100);
+  tone(buzzerPin, 1250, 100); 
+  delay(100);
+  tone(buzzerPin, 1000, 400);
+  delay(500);
+  noTone(buzzerPin);
+  tone(buzzerPin, 1000, 400);
+  delay(500);
+  noTone(buzzerPin);
+  tone(buzzerPin, 1000, 400);
+}
